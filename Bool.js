4cc3b855b76c3a9c6ea9bfa778e3940fa38c72e8
SergioBonatto <bonatto@tutanota.com>
2025-02-16 19:22:51 -0300

    update: Bool

--- Bool.js
+++ Bool.js
@@ -2,9 +2,22 @@ const True  = (t, f) => t;
 const False = (t, f) => f;
 
 const If  = (c, t, o) => c(t, o);
-const Not = b => b(False, True);
-const And = (b1, b2) => (t, f) => b1 (b2(t, f), f)
 
-console.log(And(False, True) ("true", "false"));
+const Not = b => b(False, True);
+
+const And = (a, b) => (t, f) => a (b(t, f), f)
+
+const Or = (a, b) => (t, f) => a (t, b(t, f))
+
+const Xor = (a, b) => (t, f) => a (b(f, t), b(t, f)) 
+
+const Nand = (a, b) => (t, f) => a(b(f, t), t)
+
+const Nor = (a, b) => (t, f) => a(f, b(f, t))
+
+const Xnor = (a, b) => (t, f) => a(b(t, f), b(f, t))
+
+console.log(Xnor(True, False) ("true", "false"));
+
 
 
